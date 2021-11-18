e9ff87d57ff488d156c5285e6f999ec9aee9166f
zhaozhuoboy <zhaozhuodev@163.com>
2021-11-18 09:34:48 +0800

    style(home.vue): 增加菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,9 @@
           <li>文章列表</li>
           <li>关于</li>
           <li>Github</li>
+          <li>Github</li>
+          <li>Github</li>
+          <li>gitlab</li>
         </ul>
       </nav>
     </header>
