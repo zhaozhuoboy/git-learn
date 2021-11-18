5581cf761346049c18d3004ce7f2c529f808089a
zhaozhuoboy <zhaozhuodev@163.com>
2021-11-18 09:27:14 +0800

    feat(views/Home.vue): 新增Home页面

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-home">
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-home',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
